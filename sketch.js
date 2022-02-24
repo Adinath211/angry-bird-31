6df98194d05be2faff7a6c0d4a64b00c8b1e234f
Adinath211 <74005208+Adinath211@users.noreply.github.com>
2022-02-24 20:05:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,80 +1,48 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
+const Body = Matter.Body;
+const Constraint=Matter.Constraint
 var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
-var bird, slingShot;
+var dustbinImg, dustbin;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload(){
+  dustbinImg = loadImage("dustbingreen.png");
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(1300, 700);
+  engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine);
 
+  paper = new Paper(100,520,70);
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+  ground1 = new Ground(400,550,1900,15);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+  dustbin = createSprite(1000,400,200,20);  
+  dustbin.addImage(dustbinImg);
+  dustbin.scale = 0.85;
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+  dustbin1 = new Dustbin(1000,535,180,15);
+  dustbin2 = new Dustbin(880,400,15,285);
+  dustbin3 = new Dustbin(1120,400,15,285);
+  launcher = new SlingShot(paper.body,{x:100,y:520})
+  }
+function draw() {
+  
+  rectMode(CENTER);
+  background(255);
+  
+  dustbin.display();
 
-    log3 =  new Log(810,180,300, PI/2);
+  ground1.display();
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-}
-
-
-function mouseReleased(){
-    slingshot.fly();
-}
+  paper.display();
+  launcher.display();
+ }
 function keyPressed(){
-    if(keyCode===32)
-    slingshot.attach(bird.body);
+  if(keyCode=== UP_ARROW){
+    Matter.Body.applyForce(paper.body,paper.body.position,{x:1080,y:-1080});
+  }
 }
